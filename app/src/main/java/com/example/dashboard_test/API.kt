9816f61632e6a5015fbf9c72b67330394fb56641
Joshua <jojosinogba@gmail.com>
2023-04-19 14:31:02 +0800

    Added Dashboard Files

--- app/src/main/java/com/example/dashboard_test/API.kt
+++ app/src/main/java/com/example/dashboard_test/API.kt
@@ -4,6 +4,7 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface API {
--- app/src/main/java/com/example/dashboard_test/Retrofit.kt
+++ app/src/main/java/com/example/dashboard_test/Retrofit.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object Retrofit {
     val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.1.30:3000/api/") // Replace with your cloud service's API endpoint
+        .baseUrl("http://10.0.1.14:3000/send/telegram") // Replace with your cloud service's API endpoint
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
